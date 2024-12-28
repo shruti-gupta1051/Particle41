1445116cd16feb5eab2d3ed8ed9620e2c5f54482
shruti-gupta1051 <shruti.gupta1051@gmail.com>
2024-12-28 13:16:04 +0530

    adding k8s config

--- terraform/k8-config.tf
+++ terraform/k8-config.tf
@@ -1,3 +1,7 @@
+data "aws_eks_cluster_auth" "eks_cluster" {
+  name = module.eks.cluster_id  
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
